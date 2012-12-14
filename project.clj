423b7b33d91099358c47eaeade6c498d80b2e5a8
Gabriel Horner <gabriel.horner@gmail.com>
2012-12-14 14:28:40 -0500

    generate lein marg docs with "lein marg"

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.datomic/datomic-sim "0.1.3"
   :description "Simulation testing with Datomic"
-  :dev-dependencies [[marginalia "0.3.2"]]
+  :dev-dependencies [[lein-marginalia "0.7.1"]]
+  :plugins [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.5.0-beta1"]
                  [org.clojure/test.generative "0.3.0"]
                  [com.datomic/datomic-free "0.8.3646"
