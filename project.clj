0a95b1d66a6fa166a2f7f46f73dc35fc395ffb6d
Gabriel Horner <gabriel.horner@gmail.com>
2012-12-14 14:29:54 -0500

    Let's assume lein2 for now

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject com.datomic/datomic-sim "0.1.3"
   :description "Simulation testing with Datomic"
-  :dev-dependencies [[lein-marginalia "0.7.1"]]
   :plugins [[lein-marginalia "0.7.1"]]
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.0-beta1"]
                  [org.clojure/test.generative "0.3.0"]
                  [com.datomic/datomic-free "0.8.3646"
