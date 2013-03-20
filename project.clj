da121d926c0ae6f4d026d84cd8c823efb9989631
Stuart Halloway <stu@thinkrelevance.com>
2013-03-20 07:02:22 -0700

    update deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :source-paths ["src" "examples"]
   :plugins [[lein-marginalia "0.7.1"]]
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/test.generative "0.3.0"]
-                 [com.datomic/datomic-free "0.8.3731"
+                 [com.datomic/datomic-free "0.8.3848"
                   :exclusions [org.clojure/clojure]]])
