8abde00f3616cb687d44f5bc339e7b544b953ce2
Bobby Calderwood <bobby_calderwood@mac.com>
2013-03-20 16:31:54 -0400

    Add license and URL

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject com.datomic/simulant "0.1.4"
   :description "Simulation testing with Datomic"
+  :url "https://github.com/Datomic/simulant"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :source-paths ["src" "examples"]
   :plugins [[lein-marginalia "0.7.1"]]
   :min-lein-version "2.0.0"
