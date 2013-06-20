7db1f93817d185d701dbdb644485728389e75a0e
Bobby Calderwood <bobby_calderwood@mac.com>
2013-06-20 18:29:54 -0400

    Bump version to latest snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.datomic/simulant "0.1.4"
+(defproject com.datomic/simulant "0.1.6-SNAPSHOT"
   :description "Simulation testing with Datomic"
   :url "https://github.com/Datomic/simulant"
   :license {:name "Eclipse Public License - v 1.0"
