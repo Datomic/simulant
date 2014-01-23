95de099714b0e601edf9260673a4bb42b5ef597c
David Chelimsky and Michael Nygard <pair+dc+mn@cognitect.com>
2014-01-23 13:11:22 -0500

    Update dependencies on Datomic and test.generative

--- project.clj
+++ project.clj
@@ -10,5 +10,5 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/test.generative "0.5.0"]
-                 [com.datomic/datomic-free "0.8.4020.24"
+                 [com.datomic/datomic-free "0.9.4470"
                   :exclusions [org.clojure/clojure]]])
